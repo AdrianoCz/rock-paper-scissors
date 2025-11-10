1d92ad3f5280c199c5af6bcfe507502d17631344
Adriano <235764285+AdrianoCz@users.noreply.github.com>
2025-11-10 17:42:43 -0300

    add fully functional rock paper scissors game

--- script.js
+++ script.js
@@ -12,7 +12,13 @@ function play(x) {
     document.getElementById(
       "result"
     ).innerHTML = `<p>Computer chose ${rps[machineInput]}</p><p>User chose ${rps[userInput]}</p><p>Final result: draw</p>`;
-  } else if (userInput == 0 && machineInput == 3) {
+  } else if (userInput == 0 && machineInput == 2) {
     document.getElementById("result").innerHTML = `<p>Computer chose ${rps[machineInput]}</p><p>User chose ${rps[userInput]}</p><p>Final result: User victory</p>`;
+  } else if (userInput == 1 && machineInput == 0) {
+    document.getElementById("result").innerHTML = `<p>Computer chose ${rps[machineInput]}</p><p>User chose ${rps[userInput]}</p><p>Final result: User victory</p>`;
+  } else if (userInput == 2 && machineInput == 1) {
+    document.getElementById("result").innerHTML = `<p>Computer chose ${rps[machineInput]}</p><p>User chose ${rps[userInput]}</p><p>Final result: User victory</p>`;
+  } else {
+    document.getElementById("result").innerHTML = `<p>Computer chose ${rps[machineInput]}</p><p>User chose ${rps[userInput]}</p><p>Final result: Computer victory</p>`;
   }
 }
